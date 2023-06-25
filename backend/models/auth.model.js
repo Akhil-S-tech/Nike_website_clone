1225ac5c4fedc69edb02be01e0b27add7fbf2cc6
a-khils <akhilsomanmeg@gmail.com>
2023-06-25 09:16:11 +0530

    models setup

---
+++ backend/models/auth.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const User = new mongoose.Schema({
+  firstName: { type: String, require: [true, "Require firstName"] },
+  lastName: { type: String, require: [true, "Require lastName"] },
+  email: { type: String, require: [true, "Email is missing"] },
+  gender: { type: String, require: [true, "gender is missing"] },
+  password: { type: String, require: [true, "require password"] },
+  dateOfBirth: { type: String, require: [true, "require dateOfBirth"] },
+});
+const UserModel = mongoose.model("user", User);
+
+module.exports = {
+  UserModel,
+};
---
+++ backend/models/product.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  type: { type: String, require },
+  title: { type: String, require },
+  text: { type: String, require },
+  description: { type: String, require },
+  rating: { type: Number, require },
+  images: { type: String, require },
+  price: { type: Number, require },
+  color: { type: String, require },
+  shadow: { type: String, require },
+  details: [String],
+});
+
+const ProductModel = mongoose.model("shoes", productSchema);
+
+module.exports = { ProductModel };
