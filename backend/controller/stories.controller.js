b810e0b4c0cf9e86fe0e5df5355b2746ee252b29
a-khils <akhilsomanmeg@gmail.com>
2023-06-25 09:07:35 +0530

    database config and controller setup

---
+++ backend/controller/stories.controller.js
@@ -0,0 +1,22 @@
+const { StoryModel } = require("../models/story.model");
+
+const storiesRouter = require("express").Router();
+
+exports.getStories = async (req, res) => {
+  try {
+    let stories = await StoryModel.find();
+    res.status(201).send({ msg: stories });
+  } catch {
+    res.status(404).send({ msg: [], error: "something went wrong" });
+  }
+};
+
+exports.addStories = async (req, res) => {
+  try {
+    let newData = new StoryModel(req.body);
+    newData.save();
+    res.status(201).send({ msg: "added successfully!" });
+  } catch (error) {
+    res.status(404).send({ msg: [], error: "something went wrong" });
+  }
+};
